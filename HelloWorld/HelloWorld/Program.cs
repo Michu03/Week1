598f5d59484fd83e2d1c00097747cbd3b7178d95
Michu <60574730+Michu03@users.noreply.github.com>
2020-12-02 13:31:30 +0100

    Add files via upload

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,20 +2,19 @@
 
 namespace HelloWorld
 {
-    //PascalCase -> stosowany do nazewnicta klas, nazw plików, nazw namespace oraz nazw metod/funkcji
+    //PascalCase -> stosowany do nazewnicta klas, nazw plików, nazw namespace oraz nazw metod/funkcji / used to name classes, file names, namespaces, methods/functions
     public class Program
     {
         public static void Main(string[] args)
         {
-            //camelCase -> stosowany do nazewnictwa lokalnych oraz prywatnych zmiennych
+            //camelCase -> stosowany do nazewnictwa lokalnych oraz prywatnych zmiennych / used to name locals and privates variables
             string myName = "Michał";
 
-            //UPPER_CASE -> stosowany do nazewnictwa stałych
-
+            //UPPER_CASE -> stosowany do nazewnictwa stałych / used to name constant variables
             const double PI_NUMBER = 3.14;
 
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Mam na imię" + myName);
+            Console.WriteLine("Mam na imię " + myName);
         }
     }
 }
