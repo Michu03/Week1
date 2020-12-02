e760f5fe5c66924fca481088c103ec9a2699217e
Michu <60574730+Michu03@users.noreply.github.com>
2020-12-02 13:29:42 +0100

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //PascalCase -> stosowany do nazewnicta klas, nazw plików, nazw namespace oraz nazw metod/funkcji
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //camelCase -> stosowany do nazewnictwa lokalnych oraz prywatnych zmiennych
+            string myName = "Michał";
+
+            //UPPER_CASE -> stosowany do nazewnictwa stałych
+
+            const double PI_NUMBER = 3.14;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mam na imię" + myName);
+        }
+    }
+}
